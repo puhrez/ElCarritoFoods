bea6baa13f985482e086f0f51097f018ec46c682
Michael Pérez <mpuhrez@gmail.com>
2017-09-11 22:38:23 -0400

    add quantity and shopping cart widget

---
+++ frontend/src/components/ui/Quantity.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="quantity">
+    <h4>{{ label }}: {{ quantity }}</h4>
+    <div><button :disabled="quantity === 0" @click="less">-</button><button @click="more">+</button></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'quantity',
+  props: ['label', 'quantity'],
+  methods: {
+    less () {
+      this.$emit('change', this.quantity - 1)
+    },
+    more () {
+      this.$emit('change', this.quantity + 1)
+    }
+  }
+}
+</script>
